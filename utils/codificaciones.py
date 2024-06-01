25a574a5067dd5b578d90f3c0e095ce3ea1fb249
Romina Torres <romina.torres.calderon@gmail.com>
2024-05-31 22:48:00 -0300

    Cambio el nombre de las columnas

--- utils/codificaciones.py
+++ utils/codificaciones.py
@@ -1,4 +1,4 @@
-cod_provincia_dict = {
+cod_provincia = {
     2: 'Ciudad de Buenos Aires',
     6: 'Buenos Aires',
     10: 'Catamarca',
@@ -25,7 +25,7 @@ cod_provincia_dict = {
     94: 'Tierra del Fuego'
 }
 
-region_dict = {
+region = {
     1: 'Metropolitana',
     2: 'Pampeana',
     3: 'Noroeste',
@@ -34,14 +34,14 @@ region_dict = {
     6: 'Patagónica'
 }
 
-tamanio_aglomerado_dict = {
+tamanio_aglomerado = {
     1: 'Más de 1.500.000 habitantes',
     2: '500.001 a 1.500.000 habitantes',
     3: '150.001 a 500.000 habitantes',
     4: 'Menos de 150.000 habitantes'
 }
 
-aglomerado_dict = {
+aglomerado = {
     1: 'Gran Buenos Aires',
     2: 'Gran Córdoba',
     3: 'Gran Rosario',
@@ -51,4 +51,133 @@ aglomerado_dict = {
     7: 'Mar del Plata - Batán',
     8: 'Gran Salta',
     9: 'Resto de los aglomerados'
+}
+
+periodicidad_fumador = {
+    1: '...todos los días?',
+    2: '...algunos días?',
+    3: '...no fuma?'
+}
+
+marca_cigarrillos = {
+    1: 'Lucky Strike',
+    2: 'Camel',
+    3: 'Pall Mall',
+    4: 'Marlboro',
+    5: 'Philip Morris',
+    6: '43/70',
+    7: 'Baltimore',
+    8: 'Bashia',
+    9: 'Benson & Hedges 100s',
+    10: 'Chesterfield',
+    11: 'CJ',
+    12: 'Dunhill fine cut master',
+    13: 'Freeport box',
+    14: 'Gavilán',
+    15: 'Gitanes blondes',
+    16: 'Gudang Gararm',
+    17: 'Imparciales 100s',
+    18: 'Inter gold',
+    19: 'Jockey club',
+    20: 'John Player gold leaf',
+    21: 'L&M',
+    22: 'Melbourne',
+    23: 'Milenio',
+    24: 'Paladium',
+    25: 'Parisiennes',
+    26: 'Parliament',
+    27: 'Particulares',
+    28: 'Red Point',
+    29: 'Rodeo',
+    30: 'Rothmans',
+    31: 'V8',
+    32: 'Viceroy',
+    33: 'Virginia super Slims 100s box',
+    34: 'Winston',
+    35: 'Otro'
+}
+
+tipo_paquete_comprado = {
+    1: 'Box',
+    2: 'Común (blando)',
+    98: 'Fuma cigarrillos sueltos',
+    99: 'Ns/Nc'
+}
+
+cigarrillos_sabor = {
+    1: 'Común (sin saborizar)',
+    2: 'Mentolados',
+    3: 'Otro sabor'
+}
+
+cigarrillos_tipo_capsula = {
+    1: 'Cápsula simple',
+    2: 'Cápsula doble',
+    99: 'Ns/Nc'
+}
+
+intento_dejar_fumar = {
+    1: 'Sí',
+    2: 'No',
+    99: 'Ns/Nc'
+}
+
+hubo_fumadores_casa = {
+    1: 'Sí',
+    2: 'No',
+    98: 'No estuvo'
+}
+
+hubo_fumadores_trabajo = {
+    1: 'Sí',
+    2: 'No',
+    98: 'No estuvo'
+}
+
+hubo_fumadores_escuela = {
+    1: 'Sí',
+    2: 'No',
+    98: 'No estuvo'
+}
+
+hubo_fumadores_hospital = {
+    1: 'Sí',
+    2: 'No',
+    98: 'No estuvo'
+}
+
+hubo_fumadores_otros_lugares = {
+    1: 'Sí',
+    2: 'No',
+    98: 'No estuvo'
+}
+
+publicidad_comercios = {
+    1: 'Sí',
+    2: 'No',
+    98: 'No fue a comercios donde se venden cigarrillos'
+}
+
+vio_propaganda_riesgo_fumar = {
+    1: 'Sí',
+    2: 'No',
+    98: 'No vio paquetes de cigarrillos'
+}
+
+propaganda_piensa_dejar_fumar = {
+    1: 'Sí',
+    2: 'No',
+    99: 'Ns/Nc'
+}
+
+acuerdo_impuesto_tabaco = {
+    1: 'Sí',
+    2: 'No',
+    99: 'Ns/Nc'
+}
+
+condicion_fumador = {
+    1: 'Fumador actual',
+    2: 'Ex fumador',
+    99: 'No fumador'
 }
